1c0015a9ef34be6c116a23362d9529b3d647abef
Nicholas Blott <blottn@tcd.ie>
2020-04-06 23:29:21 +0100

    allow it to be executed from anywhere

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -1,1 +1,2 @@
-ansible-playbook -v -i inventory --become-method=sudo main.yml
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ansible-playbook -v -i $DIR/inventory --become-method=sudo $DIR/main.yml
